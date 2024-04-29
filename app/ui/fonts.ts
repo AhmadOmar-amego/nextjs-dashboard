955b5de4ec36572d22413485230aa840c53440a2
Ahmad Omar <ahmadomar.amego@gmail.com>
2024-04-29 06:49:11 +0300

    upload first copy

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Handjet ,Lusitana } from 'next/font/google';
+
+export const inter = Handjet({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
